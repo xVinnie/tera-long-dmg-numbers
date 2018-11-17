5c67ed7526250e0f87e24b2b8a736d0529afc9e7
Leyki <ocsomrak@gmail.com>
2018-11-17 17:35:55 -0300

    Major updooot, please tell me if no work btw...

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### What it do
 
-It basically divides the damage you deal by the value set in the script, by the default being `10000`. This can be changed to your liking via editting the `const divisor`.
+It basically divides the damage you deal by the value set in the script, by the default being `10000`. This can be changed to your liking via editting the `divisor` value found in `tera-short-dmg-numbers\settings.json`. The module can be set to load disabled by setting `enabled` to false as well. There's also in-game commands, wow. 
+
+#### In-game Commands
+
+* ***`smn`***  *`[on/off]`* turns on and off the whole module functionalities.
+######
+* ***`smn`*** *`divisor` `new divisor value(number)`* sets a new damage divisor.
--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-module.exports = function ZelekieShortDmgNumbers(d) {
+module.exports = function ZelekieShortDmgNumbers(mod) {
 
-const dmgType = 1 
-const divisor = 10000
+  // Ponku told me to put this here
+  if (mod.proxyAuthor !== 'caali') {
+    const options = require('./module').options
+    if (options) {
+      const settingsVersion = options.settingsVersion
+      if (settingsVersion) {
+        mod.settings = require('./' + (options.settingsMigrator || 'module_settings_migrator.js'))(mod.settings._version, settingsVersion, mod.settings)
+        mod.settings._version = settingsVersion
+      }
+    }
+  }
 
-  d.hook('S_EACH_SKILL_RESULT', 12, event => {
-    if ((d.game.me.is(event.source) || d.game.me.is(event.owner)) && event.type == dmgType) {
-      event.damage = Math.round(event.damage / divisor)
+  const dmgType = 1,
+    settings = require(`./settings.json`)
+
+  mod.hook('S_EACH_SKILL_RESULT', 12, event => {
+    if (settings.enabled && (mod.game.me.is(event.source) || mod.game.me.is(event.owner)) && event.type == dmgType) {
+      event.damage = Math.round(event.damage / settings.divisor)
       return true
+    }
+  })
+
+  mod.command.add('smn', {
+    on() {
+      if (!settings.enabled) {
+        settings.enabled = true
+        mod.command.message('Module enabled.')
+      }
+      else mod.command.message('Fool, i was already enabled!')
+    },
+    off() {
+      if (settings.enabled) {
+        settings.enabled = false
+        mod.command.message('Module disabled.')
+      }
+      else mod.command.message('Fool, i was already disabled!')
+    },
+    divisor(NewValue) {
+      settings.divisor = NewValue
+      mod.command.message('New settings.divisor set to: ' + settings.divisor + '.')
     }
   })
 }
